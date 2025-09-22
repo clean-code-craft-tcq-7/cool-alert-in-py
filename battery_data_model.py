b4bd01984218b73c592300a6ea71d342b83ba1b4
Manoj Mohan <haimanojkumar@gmail.com>
2025-09-22 17:30:49 +0530

    setup the steps

--- battery_data_model.py
+++ battery_data_model.py
@@ -2,13 +2,15 @@ from dataclasses import dataclass
 from enum import Enum
 from thresholds import ThermalManagementType
 
-class BatteryState(Enum):
-    BATTERY_OK = 1
-    BATTERY_WARNING = 2
-    BATTERY_ALERT = 3
-
-@dataclass
-class BatteryDataModel:
-    battery_id: int
-    thermal_management_type: ThermalManagementType
-    temperature: float
+#----------- STEP 5 code starts ----------
+#class BatteryState(Enum):
+#    BATTERY_OK = 1
+#    BATTERY_WARNING = 2
+#    BATTERY_ALERT = 3
+#
+#@dataclass
+#class BatteryDataModel:
+#    battery_id: int
+#    thermal_management_type: ThermalManagementType
+#    temperature: float
+#----------- STEP 5 code ends ----------
