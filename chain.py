d48fb21cd1c86c566d8d4a458a512108097e1810
Manoj Mohan <haimanojkumar@gmail.com>
2025-09-23 08:31:39 +0530

    invoke classifier

--- chain.py
+++ chain.py
@@ -1,10 +1,11 @@
 from actions import Actuators
-from battery_data_model import BatteryDataModel
-from thresholds import alert_temperature_for_cooling_type
+from battery_data_model import BatteryDataModel, BatteryState
+from classify import classify_battery_state
 # ---------- STEP 6 code starts ----------
-#
+
 #def battery_data_to_action(battery_data: BatteryDataModel, actuators: Actuators):
-#    if battery_data.temperature > alert_temperature_for_cooling_type(battery_data.thermal_management_type):
+#    state = classify_battery_state(battery_data)
+#    if state == BatteryState.BATTERY_ALERT:
 #        actuators.email_sender("manager@battery.com", "Battery Alert", "Battery temperature is too high", 
 #                               "noreply@battery.com")
 #            
